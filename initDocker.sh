c155e7a19ab6b204b85bda97c2bb4bc8c6babbee
root <root@atlasHead.atlas>
2017-11-21 13:40:48 +0100

    change initDocker.sh to non background

--- initDocker.sh
+++ initDocker.sh
@@ -5,13 +5,13 @@ wait
 echo "done"
 sed -i -e "s/HOSTIP/$HOSTIP/g" /tmp/grub/grub.cfg 
 cp -rf /tmp/grub /srv/install/boot/
-/usr/bin/in.tftpd --listen --secure -l -s -vvv /srv/install/boot >> /tmp/tftpd.log &
+/usr/bin/in.tftpd --listen --secure -l -s -vvv /srv/install/boot 
 /usr/sbin/start_restserver.sh >> /tmp/restServer.log &
 #cp -f /etc/dhcpd.conf /etc/dhcp/dhcpd.conf
-/usr/bin/dhcpd -4 >> /tmp/dhcp.log &
-/usr/bin/exportfs -a >> /tmp/export.log &
-rpcbind >> /tmp/rpcBind.log &
-/usr/sbin/rpc.statd >> /tmp/rpcStatd.log &
-/usr/sbin/rpc.nfsd  >> /tmp/rpcNfsd.log&
+/usr/bin/dhcpd -4  
+/usr/bin/exportfs -a  
+rpcbind  
+/usr/sbin/rpc.statd  
+/usr/sbin/rpc.nfsd  
 rpc.mountd --foreground 
 wait
